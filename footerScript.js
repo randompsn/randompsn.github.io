d5fe94be26a53eb89daaa0aad5fdaa7fb573a0f2
Randompsn <anniewang0411@gmail.com>
2026-09-15 17:33:40 -0700

    added functionality for random footer

--- footerScript.js
+++ footerScript.js
@@ -1,3 +1,33 @@
-function writeFooter{
-    document.write("")
-}
+function randomQuote(){
+                        let qNumber = Math.floor(Math.random() * 4) + 1;
+                        console.log(qNumber);
+                        switch(qNumber){
+                            case 1:{
+                                return `<span>Trillions of choices and one final outcome.</span>`;
+                                break;
+                            }case 2:{
+                                return `<span lang="cn">選ぶ、私が!</span>`;
+                                break;
+                            }case 3:{
+                                return `<span lang="cn">将一切忘掉，然后歇斯底里地回想。</span>`;
+                                break;
+                            }case 4:{
+                                return `<span>It's only I and science that never leaves me.</span>`;
+                                break;
+                            }case 5:{
+                                return `<span>Despite everything, it's still me.</span>`;
+                                break;
+                            }
+                        }
+                    }
+                    document.write(`<footer>
+                        <div class="footer-quote">
+                                    ${randomQuote()}
+                                    </div>
+                                    <hr>
+                                    Randompsn ©2025-2026. All rights reserved.
+                                </footer>`);
+
+                    
+               
+            
